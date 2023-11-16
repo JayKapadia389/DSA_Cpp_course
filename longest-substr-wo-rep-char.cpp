bf87a478e6a65ad455513bdb1fc17deb9c5ea263
Jay <jaykapadia389@gmail.com>
2023-11-17 00:11:11 +0530

    longest sub-string without repeting characters problem

---
+++ longest-substr-wo-rep-char.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int getLen(string s){
+
+    queue<pair<char,int>> q;
+    int mx = 0 , len = 0; 
+
+    for(int i = 0 ; i < s.length() ; i++){
+
+        for(int j = 0 ; j < q.size() ; j++){
+            
+        }
+    }
+}
+
+int main(){
+
+    string s = "";
+
+    cout << getLen(s);
+
+}
