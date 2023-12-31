b85252dc7403dd32e862f47953c295552ccdb090
Jay <jaykapadia389@gmail.com>
2023-12-31 23:55:10 +0530

    sudoku solver -leetcode problem

--- exp.cpp
+++ exp.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <set>
+#include <stack>
 
 using namespace std ;
 
+set<int> fun(vector <int> v){
+
+   set<int> s;
+
+   for(int i = 0 ; i < v.size() ; i++){
+
+       s.insert(v[i]);       
+   }
+
+   return s ;
+
+}
+
 int main(){
+
+    stack<string> st ;
 
    // cout << static_cast<int>('-') << static_cast<int>('+') << static_cast<int>('*') << " " << static_cast<int>('/');
 
@@ -15,15 +32,13 @@ int main(){
    // int  arr[] = {0};
    // int  arr[7];
 
-   queue<int> q ;
+   // queue<int> q ;
 
-   q.push(2);
-   q.push(3);
-   q.push(5);
+   // q.push(2);
+   // q.push(3);
+   // q.push(5);
 
-   cout << q.size();
-
-
+   // cout << q.size();
 
    // int * arr = new int[20];
 
@@ -34,10 +49,26 @@ int main(){
    //    cout << arr[i] << " ";
    // }
 
-
-
    // for(int i = 0 ; i < 256 ; i++){
 
    //    cout << static_cast<char>(i) << endl;
    // }
+
+   vector<int> v = {1,2,3,7,8};
+
+   set<int> s = fun(v);
+   set<string> num= {"1" , "s" , "dh"};
+
+   for(auto i : s){
+        cout << i << " " ;
+    }
+
+    cout << endl ;
+
+    s.erase(7);
+
+    for(auto i : s){
+        cout << i << " " ;
+    }
+
 }
