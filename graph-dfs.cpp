37b7886a34436b399d11db5f0d841e337dfb7f0b
Jay <jaykapadia389@gmail.com>
2024-01-25 23:59:35 +0530

    fixed dfs , started topological sort

--- graph-dfs.cpp
+++ graph-dfs.cpp
@@ -8,31 +8,44 @@ int main(){
 
     int n , m ;
 
-    cin >> n >> m ;
+    // cin >> n >> m ;
+    n = 7 ,  m = 7 ;
 
-    vector<vector<int>> adjm(n+1 , <vector<int>(n+1 , 0)) ;
+    vector<vector<int>> adjm(n+1 , vector<int>(n+1 , 0)) ;
     int x , y ;
 
-    for(int i = 0 ; i < m ; i++){
+    // for(int i = 0 ; i < m ; i++){
 
-        cin >> x >> y ;
+    //     cin >> x >> y ;
 
-        adjm[x][y] = 1 ;
-        adjm[y][x] = 1 ;
+    //     adjm[x][y] = 1 ;
+    //     adjm[y][x] = 1 ;
 
-    }
+    // }
+
+    adjm = {{0,0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0},
+            {0,1,0,0,1,1,1,1},
+            {0,1,0,0,0,0,0,1},
+            {0,0,1,0,0,0,0,0},
+            {0,0,1,0,0,0,0,0},
+            {0,0,1,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0}} ;
+    
 
     stack<int> s ;
     int vis[n+1] = {0};
+    bool flag = 0 ;
 
     int a = 3 ;
-    s.push(3);
-    vis[s] = 1 ;
+    s.push(a);
+    vis[a] = 1 ;
     cout << a << " ";
 
     while(!s.empty()){
 
         a = s.top() ;
+        flag = 0 ;
 
         for(int i = 1 ; i <= n ;i++){
             if(adjm[a][i] && !vis[i]){
@@ -41,9 +54,14 @@ int main(){
                 vis[i] = 1;
                 cout << i << " ";
 
+                flag = 1;
+                break ;
             }
         }
 
-        s.pop();
+        if(!flag){
+         s.pop();
+        }
     }
+
 }
