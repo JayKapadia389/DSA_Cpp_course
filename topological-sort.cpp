6d8a7d9cfef03a330ae2212fddfcd8c3754462bf
Jay <jaykapadia389@gmail.com>
2024-01-26 23:55:14 +0530

    topological sort , started cycle detection

--- topological-sort.cpp
+++ topological-sort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -6,5 +8,53 @@ int main(){
 
     int n , m ;
 
-    
+    cin >> n >> m ;
+
+    vector<int> indeg(n+1);
+    vector<vector<int>> adjl(n+1) ;
+
+    int x , y;
+
+    for(int i = 0 ; i < m ; i++){
+        cin >> x >> y ;
+
+        adjl[x].push_back(y);
+        indeg[y]++ ;
+    }
+
+    queue<int> q ;
+    // map<int , int > :: iterator mit ;
+    vector<int> :: iterator vit ;
+
+    for(int i = 1 ; i <= n; i++){
+        // cout << "1" ;
+        if(indeg[i] == 0){
+            // cout << "2" ;
+            q.push(i);
+            cout << "push" << i << endl; 
+        }
+    }
+
+    int curr ;
+
+    if(q.empty()){
+        cout << "graph is cyclic" ;
+    }
+
+    while(!q.empty()){
+
+        curr = q.front();
+        cout << curr << " " ;
+        q.pop();
+
+        for(vit = adjl[curr].begin() ; vit != adjl[curr].end() ; vit++){
+            
+            indeg[*vit]--;
+
+            if(indeg[*vit] == 0){
+                q.push(*vit);
+            }
+        }
+
+    }
 }
--- vectors.cpp
+++ vectors.cpp
@@ -7,32 +7,32 @@ int main(){
 
     // vector is expandable
     // if length is predefined 
-    vector<int> v; 
+//     vector<int> v; 
 
-   v.push_back(1);
-   cout << v[0];
-   v.pop_back(); // data can be added at defined spots
+//    v.push_back(1);
+//    cout << v[0];
+//    v.pop_back(); // data can be added at defined spots
   
     // length is not defined
 
-    // vector<int> a;
+    vector<int> a;
 
-    // a.push_back(1); // data can be added by push_back
-    // a.push_back(2);
-    // a.push_back(3);
-    // a.push_back(7);
+    a.push_back(1); // data can be added by push_back
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(7);
 
-    // vector<int>::iterator it;
+    vector<int>::iterator it;
 
-    // it = v.begin() ; 
+    it = a.begin() ; 
 
-    // cout << *it ;
-    // cout << *(it+1) ;
+    cout << *it ;
+    cout << *(it+1) ;
 
     // a.pop_back();
     // a.pop_back();
 
-    cout << v.size()  ;
+    // cout << v.size()  ;
 
     // v.pop_back();
     // v.pop_back();
