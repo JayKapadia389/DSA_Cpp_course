e80efef42cf6075ef66fef5a8cc6dda32c486e98
Jay <jaykapadia389@gmail.com>
2024-01-30 23:59:29 +0530

    prims algorithm

---
+++ graph-prims-algo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <set> // minheap
+
+using namespace std;
+
+int main(){
+
+    int n , m ;
+
+    cin >> n >> m ;
+
+    vector<vector<int>> adj ;
+    set<vector<int>> edges; // {w , u , v}
+
+    int x , y , w;
+
+    for(int i = 0 ; i < m ; i++){
+
+        cin >> w >> x >> y;
+
+        adj[x] 
+    }
+    
+}
