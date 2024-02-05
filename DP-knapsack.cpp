8c1c4b187c7182a61770de4c38c0cdafd42e9759
Jay <jaykapadia389@gmail.com>
2024-02-05 23:58:24 +0530

    DP - perfect squares ,coin change , knapsack

---
+++ DP-knapsack.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int val = 0 , wt = 0 ;
+
+void knapsack(int currVal , int currWt , vector<bool > taken , int idx , vector<pair<int,int>> v , int W){
+    cout << "1" ;
+
+    if(idx != -1){
+
+        taken[idx] = 1;
+
+        if(currVal + v[idx].first > val){
+            cout << "2" ;
+            val = currVal + v[idx].first ;
+            wt = currWt + v[idx].second ;
+        }
+    }
+
+    for(int i = 0 ; i < taken.size() ; i++){
+
+        if(!taken[i]){
+
+            if((currWt + v[i].second) <= W){
+                knapsack(currVal + v[i].first , currWt + v[i].second , taken , i , v , W);
+            }
+        }
+    }
+
+    return ;
+
+}
+
+int main(){
+
+    int n ;
+    cin >> n ;
+
+    vector<pair<int,int>> v(n) ; // value , weight
+    vector<bool> taken(n , 0);
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> v[i].first  ;
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> v[i].second  ;
+    }
+    
+
+    int W ;
+    cin >> W ;
+
+    knapsack(0 , 0 , taken , -1 , v , W) ;
+
+    cout << val << " " << wt ;
+
+}
